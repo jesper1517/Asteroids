b99e20cc521f43b6e67cf339f9d6a4766c8df9fb
Jeppaz <jesper1517@hotmail.com>
2026-04-22 23:38:51 +0200

    Going to sleep for the night

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from constants import *
 
 def main():
     from logger import log_state
+
     pygame.init()
+
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -18,6 +24,8 @@ def main():
             
         screen.fill(0)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
