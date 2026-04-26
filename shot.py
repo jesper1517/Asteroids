efeeabf28f021c4a94a77392d8f25f7c61a3c6fc
Jeppaz <jesper1517@hotmail.com>
2026-04-26 23:00:07 +0200

    Added max lifetime on shots

--- shot.py
+++ shot.py
@@ -5,6 +5,7 @@ from constants import LINE_WIDTH
 class Shot(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+    self.lifetime = SHOT_LIFETIME_SECONDS
 
     def draw(self, screen):
         pygame.draw.circle(
@@ -18,3 +19,7 @@ class Shot(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity*dt
+        self.lifetime -= dt
+
+        if self.lifetime <= 0:
+            self.kill()
